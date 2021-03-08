8ebe40012dfc985cf8c60ef340be68400a388ced
Vijay <vijaythevirtue@gmail.com>
2021-03-08 13:25:28 +0530

    quick select top images added

--- lib/Screens/DashboardPage/Widgets/5QuickSelect.dart
+++ lib/Screens/DashboardPage/Widgets/5QuickSelect.dart
@@ -1,10 +1,13 @@
 import 'package:bojana/Declarations/Images/DashBoard/ImageFiles.dart';
+import 'package:bojana/GeneralWidgets/GWidget.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Widget buildQuickSelect() => Container(
       child: Column(
         children: [
           buildRow1(),
+          heightSpacer(20.5),
           buildRow2(),
         ],
       ),
@@ -12,25 +15,44 @@ Widget buildQuickSelect() => Container(
 
 Widget buildRow1() => Container(
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          buildItem("Fresh", dashboardImages[0]),
+          buildItem("Fresh", dashboardImages[2]),
+          buildItem("Gravy", dashboardImages[4]),
+          buildItem("Masala", dashboardImages[6]),
+          buildItem("Veggy", dashboardImages[7]),
         ],
       ),
     );
 
-Widget buildRow2() => Container();
+Widget buildRow2() => Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          buildItem("Non-Veg", dashboardImages[1]),
+          buildItem("Healthy", dashboardImages[5]),
+          buildItem("Dry", dashboardImages[7]),
+          buildItem("Crunch", dashboardImages[0]),
+        ],
+      ),
+    );
 
 Widget buildItem(String itemName, String imagePath) => Container(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            width: 150,
-            height: 150,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(150)),
-            child: Image.asset(
-              imagePath,
-              fit: BoxFit.contain,
+          CircleAvatar(
+            backgroundColor: Colors.transparent,
+            foregroundColor: Colors.transparent,
+            backgroundImage: AssetImage(imagePath),
+            radius: 37,
+          ),
+          heightSpacer(5.50),
+          Text(
+            itemName,
+            style: GoogleFonts.lora(
+              textStyle:
+                  TextStyle(fontSize: 12.5, fontWeight: FontWeight.w500),
             ),
           ),
         ],
