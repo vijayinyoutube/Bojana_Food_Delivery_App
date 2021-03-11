c057f38eec397cf015398e4e00088c278679d7eb
Vijay <vijaythevirtue@gmail.com>
2021-03-11 18:20:00 +0530

    code refactoring

--- lib/Declarations/DashboardDeclr.dart
+++ lib/Declarations/DashboardDeclr.dart
@@ -5,3 +5,9 @@ Color offerColor = HexColor("#1E00D7");
 
 Color assuredContainerColor = HexColor("#A3ACFF");
 Color assuredCTextColor = HexColor("#021578");
+
+Color comboContainerColor = HexColor("#A4FFA3");
+Color comboCTextColor = HexColor("#0B7802");
+
+Color mustTryContainerColor = HexColor("#FBB2D5");
+Color mustTryCTextColor = HexColor("#95007D");
